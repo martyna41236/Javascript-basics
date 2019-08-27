e9a41d3e7ba09f40877d9ceab1190b58509c2dbb
Martyna <44909737+martyna41236@users.noreply.github.com>
2019-08-27 21:42:18 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//excercise 1
 function BMIcalc()
     {
         var weight = document.getElementById("weight").value;
@@ -5,3 +6,11 @@ function BMIcalc()
         var bmi = weight/(height/100*height/100);
         document.getElementById("result").innerHTML = "Your BMI is " + bmi;
     }
+
+//excercise 2
+var quote = "Honesty is the first chapter in the book of wisdom.";
+quote.length;
+console.log (quote.slice(quote.length-7,quote.length-1));
+console.log (quote.slice (0,quote.length/2));
+console.log (quote.slice(quote.length-1,quote.length));
+
