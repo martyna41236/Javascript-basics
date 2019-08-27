9208be5fe5950001c61c550bff194f72881ae7c4
Martyna <44909737+martyna41236@users.noreply.github.com>
2019-08-27 08:11:53 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-function BMI()
+function BMIcalc()
     {
-        var weight
-        var height
-        var bmi
+        var weight = document.getElementById("weight").value;
+        var height = document.getElementById("height").value;
+        var bmi = weight/(height/100*height/100);
+        document.getElementById("result").innerHTML = "Your BMI is " + bmi;
     }
