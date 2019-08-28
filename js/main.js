fef9c00d7325f61e8a44ad86d858be954f1f01b5
Martyna <44909737+martyna41236@users.noreply.github.com>
2019-08-28 21:56:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,14 +14,42 @@ console.log (quote.slice(quote.length-7,quote.length-1));
 console.log (quote.slice (0,quote.length/2));
 console.log (quote.slice(quote.length-1,quote.length));
 
-function fun2(quote)
+function everyThird(quote)
 {
     for (i= Math.round(quote.length/2); i<= quote.length; i+=3)
     {
      console.log (quote[i]);
     }
 };
-fun2(quote);
+everyThird(quote);
+
 
 const ReverseString = quote => [...quote].reverse().join(''); console.log (ReverseString(quote));
 console.log (quote.replace(/wisdom/, "friendship"));
+
+
+function fun(quote)
+{
+    for (i= 0; i<= Math.round(quote.length/2); i+=2)
+    {
+     console.log (quote[i]);
+    }
+};
+fun(quote);
+
+//excercise 3
+let words = ['ananas', 'kobyla ma maly bok', 'bananab'];
+function palindrome(words)
+{
+    let isPalindrom = true;
+    for(let i=0; i<words.length/2; i++)
+    {
+        if(words[i] !==words[words.length-i-1])
+        {
+            isPalindrome=false;
+            break;
+        }
+    }
+    return isPalindrome;
+}
+console.log(palindrome(words));
